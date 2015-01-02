90bf3081da64cb03771d6a0a04d71d99a1b3cc31
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2015-01-02 10:58:48 +0900

    add site

---
+++ js/headfoot.js
@@ -0,0 +1,43 @@
+function header(){
+document.write('\
+<a href="index"><header>\
+	<h1>水工祭</h1>\
+	<span>～終わりなき革命～ Out of control 水工Z伝説</span>\
+<hr>\
+日時：平成24年11月3日(土)12：30 ～ 15：30<br>\
+　　　平成24年11月4日(日) 9：30 ～ 15：00<br>\
+場所：茨城県立水戸工業高等学校<br><br>タップするとTopページに移動出来ます\
+</header></a>\
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.mito-th.ed.jp/suiko-fes/" data-text="水工祭なうです！" data-lang="ja" data-size="large" data-hashtags="水工祭">ツイート</a>\
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>\
+');
+}
+
+function footer(){
+document.write('\
+<a href="index.html"><footer>\
+<span>タップするとTopページに移動出来ます</span><br><br>\
+Copyright 2012 茨城県立水戸工業高等学校 All Rights Reserved.\
+</footer></a>\
+');
+}
+
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35533639-1']);
+  _gaq.push(['_setDomainName', 'makkysnote.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+
+
+
+
+
